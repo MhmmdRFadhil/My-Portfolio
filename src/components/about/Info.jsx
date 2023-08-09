2243774ff00516224046333906c2fba5f7d95a7a
MhmmdRFadhil <fadhiill2719@gmail.com>
2023-08-09 09:36:13 +0700

    style(info): change info data

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -5,20 +5,20 @@ const Info = () => {
     <div className="about__info grid">
       <div className="about__box">
         <i class="bx bx-award about__icon"></i>
-        <h3 className="about__title">Lorem ipsum</h3>
-        <span className="about__subtitle">Lorem ipsum</span>
+        <h3 className="about__title">Experience</h3>
+        <span className="about__subtitle">Fresh Graduate</span>
       </div>
 
       <div className="about__box">
         <i class="bx bx-briefcase-alt about__icon"></i>
-        <h3 className="about__title">Lorem ipsum</h3>
-        <span className="about__subtitle">Lorem ipsum</span>
+        <h3 className="about__title">Completed</h3>
+        <span className="about__subtitle">10+</span>
       </div>
 
       <div className="about__box">
         <i class="bx bx-support about__icon"></i>
-        <h3 className="about__title">Lorem ipsum</h3>
-        <span className="about__subtitle">Lorem ipsum</span>
+        <h3 className="about__title">Support</h3>
+        <span className="about__subtitle">Online 24/7</span>
       </div>
     </div>
   );
